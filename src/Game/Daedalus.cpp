301c78ac922aa31a1dcb22fdf60945ca0553555d
avraal <gammaclone@gmail.com>
2019-01-23 19:25:34 +0200

    Create new level - map editor

---
+++ src/Game/Daedalus.cpp
@@ -0,0 +1,8 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+//
+// Created by andrew on 22.01.19.
+//
+
+#include "Daedalus.hpp"
---
+++ src/Game/Daedalus.hpp
@@ -0,0 +1,13 @@
+//
+// Created by andrew on 22.01.19.
+//
+
+#ifndef PROJECTOR_DAEDALUS_HPP
+#define PROJECTOR_DAEDALUS_HPP
+
+class Daedalus
+{
+
+};
+
+#endif //PROJECTOR_DAEDALUS_HPP
